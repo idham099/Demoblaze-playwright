13c7b066cff3e4073dff4d3c5fd8fd4c2a1f2d79
idham099 <ainul.idham_sttpln@yahoo.com>
2025-12-16 16:32:50 +0800

    commit 2

--- pageobjects/LoginPage.js
+++ pageobjects/LoginPage.js
@@ -10,10 +10,11 @@ class LoginPage {
         this.usernameInput = page.locator('#loginusername');
         this.passwordInput = page.locator('#loginpassword');
         this.loginButton = page.locator('button[onclick="logIn()"]');
+        this.logoutLink = page.locator('#logout2');
         this.welcomeMessage = page.locator('#nameofuser'); // Locator untuk verifikasi sukses
     }
 
-    // --- Actions (Fungsi Halaman) ---
+    // --- Performance Test---
     async openLoginModal() {
         const metrics = await this.page.evaluate(async () => {
             const [entry] = performance.getEntriesByType('navigation'); 
@@ -23,23 +24,51 @@ class LoginPage {
             const dnsLookupTime = timing.domainLookupEnd - timing.domainLookupStart;
             return { pageLoadTime, dnsLookupTime };
         });
-
         await this.loginLink.click();
-
         return metrics;
-
     }
 
-    async executeLogin(username, password) {
-        await this.usernameInput.fill(username);
-        await this.passwordInput.fill(password);
-        await this.loginButton.click();
+
+    //Daftar UI Test Case    
+    //TC_L001
+    async loginSuccessfully(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
     }
-   
     async verifySuccessfulLogin(username) {
         await this.page.waitForSelector('#nameofuser', { state: 'visible', timeout: 15000 }); 
         await expect(this.welcomeMessage).toHaveText(`Welcome ${username}`);
     }
-}
+   
 
+    //TC_L002
+    async loginAndVerifyFailure(username, password) {
+    const dialogPromise = this.page.waitForEvent('dialog', { timeout: 5000 }); 
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+    try {
+        const dialog = await dialogPromise;
+        const message = dialog.message();
+        console.log(`Alert Pop-up Ditemukan: ${message}`);
+        await dialog.accept();
+        return message; 
+    } catch (e) {
+        throw new Error("Login failed, but no expected error dialog appeared within the timeout.");
+        }
+    }
+
+    
+    //TC_L003
+    async clickLogout() {
+    await this.logoutLink.click();
+    }
+    async verifySuccessfulLogout() {
+    await expect(this.welcomeMessage).toBeHidden(); 
+    await expect(this.loginLink).toBeVisible(); 
+    }
+
+
+}
 module.exports = LoginPage;
