b5bf45e43f138a1125d49c773538fd60d35a7ddf
idham099 <ainul.idham_sttpln@yahoo.com>
2025-12-25 14:27:49 +0800

    sampai categories

---
+++ pageobjects/CategoryPage.js
@@ -0,0 +1,30 @@
+class CategoryPage {
+    constructor(page) {
+        this.page = page;
+        this.categoryMenu = (name) => page.locator('.list-group-item', { hasText: name });
+        this.productItem = (name) => page.locator(`//a[normalize-space()="${name}"]`);
+    }
+
+    async selectCategory(categoryName, expectedProductName) {
+        await this.page.waitForSelector('.list-group-item');
+
+        const startTime = await this.page.evaluate(() => performance.now());
+
+        const menu = this.categoryMenu(categoryName);
+        await menu.waitFor({ state: 'visible' }); 
+        await menu.click();
+        
+        const item = this.productItem(expectedProductName);
+        await item.waitFor({ state: 'visible', timeout: 10000 });
+
+        const endTime = await this.page.evaluate(() => performance.now());
+        const ajaxLoadTime = Math.round(endTime - startTime);
+        
+        await this.page.waitForTimeout(1500);
+        
+        return { pageLoadTime: ajaxLoadTime };
+    }
+
+}
+
+module.exports = CategoryPage;
